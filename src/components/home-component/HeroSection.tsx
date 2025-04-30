27063933a780a81c92216c0fce3dad3c02cf93a6
sanskardhungana2001 <s.dhungana@gmmgames.com>
2025-04-30 17:22:39 +0545

    Added user image

--- src/components/home-component/HeroSection.tsx
+++ src/components/home-component/HeroSection.tsx
@@ -2,24 +2,35 @@
 import Image from "next/image";
 import React from "react";
 
-export default function HeroSection(){
+export default function HeroSection() {
     const scrollToContact = () => {
         const contactSection = document.getElementById("contact");
         if (contactSection) {
-          contactSection.scrollIntoView({ behavior: "smooth" });
+            contactSection.scrollIntoView({ behavior: "smooth" });
         }
-      };
-    return(
+    };
+    return (
         <div>
             <div className="flex space-x-4">
                 <div className="flex-1">
                     <p className="text-[32px] font-[700]">Sanskar is a <span className="text-[#C778DD]">Full Stack Developer</span></p>
                     <p className="text-[#ABB2B] text-[16px] mt-8">He builds innovative, full-stack solutions that blend cutting-edge technology with practical problem-solving. </p>
-                    <button className="border border-[#C778DD] rounded-md py-2 px-4 mt-4 hover:opacity-80 cursor-pointer"   onClick={scrollToContact} >Contact me !!</button>
+                    <button className="border border-[#C778DD] rounded-md py-2 px-4 mt-4 hover:opacity-80 cursor-pointer" onClick={scrollToContact} >Contact me !!</button>
                 </div>
                 <div className="flex-1">
-                    <Image src="/hero-section/style.svg" width={155} height={155} alt="Style"/>
+                    <Image src="/hero-section/style.svg" width={155} height={155} alt="Style" />
                 </div>
+                <div className="flex-1 flex items-center justify-center">
+                    <Image
+                        src="/hero-section/user.png"
+                        width={300}
+                        height={300}
+                        alt="User"
+                        className="rounded-2xl mix-blend-lighten shadow-lg opacity-90"
+                    />
+                </div>
+
+
             </div>
         </div>
     )
