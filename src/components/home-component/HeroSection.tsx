9fe9d43762b03275675d7fc05bb9192bb1844597
sanskar2057 <sanskar2057@gmail.com>
2025-04-30 17:56:18 +0545

    Additonal changes

--- src/components/home-component/HeroSection.tsx
+++ src/components/home-component/HeroSection.tsx
@@ -9,17 +9,10 @@ export default function HeroSection() {
             contactSection.scrollIntoView({ behavior: "smooth" });
         }
     };
+
     return (
         <div>
-            <div className="flex space-x-4">
-                <div className="flex-1">
-                    <p className="text-[32px] font-[700]">Sanskar is a <span className="text-[#C778DD]">Full Stack Developer</span></p>
-                    <p className="text-[#ABB2B] text-[16px] mt-8">He builds innovative, full-stack solutions that blend cutting-edge technology with practical problem-solving. </p>
-                    <button className="border border-[#C778DD] rounded-md py-2 px-4 mt-4 hover:opacity-80 cursor-pointer" onClick={scrollToContact} >Contact me !!</button>
-                </div>
-                <div className="flex-1">
-                    <Image src="/hero-section/style.svg" width={155} height={155} alt="Style" />
-                </div>
+            <div className="flex flex-col-reverse sm:flex-col-reverse md:flex-col-reverse lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
                 <div className="flex-1 flex items-center justify-center">
                     <Image
                         src="/hero-section/user.PNG"
@@ -29,9 +22,24 @@ export default function HeroSection() {
                         className="rounded-2xl mix-blend-lighten shadow-lg opacity-90"
                     />
                 </div>
-
-
+                <div className="flex-1">
+                    <p className="text-[32px] font-[700]">
+                        Sanskar is a <span className="text-[#C778DD]">Full Stack Developer</span>
+                    </p>
+                    <p className="text-[#ABB2B] text-[16px] mt-8">
+                        He builds innovative, full-stack solutions that blend cutting-edge technology with practical problem-solving.
+                    </p>
+                    <button
+                        className="border border-[#C778DD] rounded-md py-2 px-4 mt-4 hover:opacity-80 cursor-pointer"
+                        onClick={scrollToContact}
+                    >
+                        Contact me !!
+                    </button>
+                </div>
+                <div className="flex-1">
+                    <Image src="/hero-section/style.svg" width={155} height={155} alt="Style" />
+                </div>
             </div>
         </div>
-    )
+    );
 }
